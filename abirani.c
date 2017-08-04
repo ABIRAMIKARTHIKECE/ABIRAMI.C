ca218b48ff234c4593179adb922bebfbae03f204
ABIRAMIKARTHIKECE <30343715+ABIRAMIKARTHIKECE@users.noreply.github.com>
2017-08-04 16:30:42 +0530

    Create abirani.c

---
+++ abirani.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+printF("the given number is positive");
+else if(a<0)
+printf("the given number is negative");
+else
+printf("the given number is zero");
+}
+

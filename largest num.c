d3d19d00236fb8f78894de19c78af89390ceb156
ABIRAMIKARTHIKECE <30343715+ABIRAMIKARTHIKECE@users.noreply.github.com>
2017-08-12 16:44:23 +0530

    Create largest num.c

---
+++ largest num.c
@@ -0,0 +1,13 @@
+#include,stdio.h>
+void main()
+{
+double n1,n2,n3;
+printf("enter the  3 number ");
+scanf("%f,%f,%f",&n1,&n2,&n3);
+if(n1>=n2 &&n1>n30
+printsf("largest num",n1);
+else if(n2>=n1 &&n2>=n3)
+printf("largest num ",n2);
+else
+printr("largest num ",n3);
+}

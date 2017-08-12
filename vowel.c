e30af4b41e2131e3573d0bbb7ba931f5666895c8
ABIRAMIKARTHIKECE <30343715+ABIRAMIKARTHIKECE@users.noreply.github.com>
2017-08-12 16:30:27 +0530

    vowel

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char ch;
+printf("enter the charactor");
+scanf("%c",ch);
+if(ch=='a' ||ch=='e' || ch=='i' ||ch=='o' ||ch=='u');
+printf("charactor is vowel");
+else
+printf("constant");
+}

6ddf921a6c1c09242b58bd335bebebc1af1310d2
ABIRAMIKARTHIKECE <30343715+ABIRAMIKARTHIKECE@users.noreply.github.com>
2017-08-12 16:36:50 +0530

    Create alphabet .c

---
+++ alphabet .c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int ch;
+printf("enter the charactor");
+scanf("%c",ch);
+if((c>='a' && c<='z')||(c>='A'&&c<='Z'))
+printf("alphabet");
+else
+printf("not alphabet");
+}
